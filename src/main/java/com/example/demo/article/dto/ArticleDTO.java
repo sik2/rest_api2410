4b2f35d95407c29937c5c838c58ef8fafbc6b125
sik2 <bickwang123@gmail.com>
2024-10-28 10:27:36 +0900

    update yml

---
+++ src/main/java/com/example/demo/article/dto/ArticleDTO.java
@@ -0,0 +1,23 @@
+package com.example.demo.article.dto;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.time.LocalDateTime;
+
+public class ArticleDTO {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String subject;
+
+    private String content;
+
+    private LocalDateTime createdDate;
+    @LastModifiedDate
+    private LocalDateTime modifiedDate;
+}
