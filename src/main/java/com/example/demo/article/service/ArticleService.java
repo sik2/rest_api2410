da35bffebc420b6900459bcdea4ba114d419d9f1
sik2 <bickwang123@gmail.com>
2024-10-29 10:44:53 +0900

    14

--- src/main/java/com/example/demo/article/service/ArticleService.java
+++ src/main/java/com/example/demo/article/service/ArticleService.java
@@ -1,7 +1,32 @@
 package com.example.demo.article.service;
 
+import com.example.demo.article.dto.ArticleDTO;
+import com.example.demo.article.entity.Article;
+import com.example.demo.article.repository.ArticleRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@RequiredArgsConstructor
 @Service
 public class ArticleService {
+    private final ArticleRepository articleRepository;
+    public List<ArticleDTO> getList() {
+        List<Article> articleList = this.articleRepository.findAll();
+
+        List<ArticleDTO> articleDTOList = articleList.stream()
+                .map(article -> new ArticleDTO(article))
+                .collect(Collectors.toList());
+
+        return articleDTOList;
+    }
+
+    public ArticleDTO getArticle(Long id) {
+        Optional<Article> optionalArticle = this.articleRepository.findById(id);
+
+        return optionalArticle.map(article -> new ArticleDTO(article)).orElse(null);
+    }
 }
