33bbc38cca5e8ece5a03b901ad0a2ceb32c47655
sik2 <bickwang123@gmail.com>
2024-11-04 12:56:28 +0900

    me api

--- src/main/java/com/example/demo/domain/member/controller/ApiV1MemberController.java
+++ src/main/java/com/example/demo/domain/member/controller/ApiV1MemberController.java
@@ -7,12 +7,14 @@ import com.example.demo.domain.member.service.MemberService;
 import com.example.demo.global.RsData.RsData;
 import com.example.demo.global.jwt.JwtProvider;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Map;
 
 
 @RestController
@@ -30,12 +32,30 @@ public class ApiV1MemberController {
     }
 
     @PostMapping("/login")
-    public RsData<?> login (@Valid @RequestBody MemberRequest memberRequest) {
+    public RsData<MemberResponse> login (@Valid @RequestBody MemberRequest memberRequest, HttpServletResponse res) {
 
         Member member = this.memberService.getMember(memberRequest.getUsername());
-        // accessToken 발급
-        String token = jwtProvider.genAccessToken(member);
+
+        String accessToken = jwtProvider.genAccessToken(member);
+        res.addCookie(new Cookie("accessToken", accessToken));
         
-        return RsData.of("200", "토큰 발급 성공", token);
+        return RsData.of("200", "토큰 발급 성공: " + accessToken , new MemberResponse(member));
+    }
+
+    @GetMapping("/me")
+    public RsData<MemberResponse> me (HttpServletRequest req) {
+        Cookie[] cookies = req.getCookies();
+        String accessToken = "";
+        for (Cookie cookie : cookies) {
+            if ("accessToken".equals(cookie.getName())) {
+                accessToken = cookie.getValue();
+            }
+        }
+
+        Map<String, Object> claims =  jwtProvider.getClaims(accessToken);
+        String username = (String) claims.get("username");
+        Member member = this.memberService.getMember(username);
+
+        return RsData.of("200", "내 회원정보", new MemberResponse(member));
     }
 }
