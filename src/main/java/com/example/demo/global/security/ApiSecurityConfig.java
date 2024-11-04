5a2e36b8eee1b2bdbf8059136d534387d7bb4c3a
sik2 <bickwang123@gmail.com>
2024-11-04 13:39:56 +0900

    jwt filter

--- src/main/java/com/example/demo/global/security/ApiSecurityConfig.java
+++ src/main/java/com/example/demo/global/security/ApiSecurityConfig.java
@@ -8,11 +8,13 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class ApiSecurityConfig {
+    private final JwtAuthorizationFilter jwtAuthorizationFilter;
     @Bean
     SecurityFilterChain apifilterChain(HttpSecurity http) throws Exception {
         http
@@ -23,6 +25,7 @@ public class ApiSecurityConfig {
                                 .requestMatchers(HttpMethod.GET, "/api/*/articles/*").permitAll()
                                 .requestMatchers(HttpMethod.POST, "/api/*/members/login").permitAll() // 로그인은 누구나 가능, post 요청만 허용
                                 .requestMatchers(HttpMethod.GET, "/api/*/members/me").permitAll() // 로그인은 누구나 가능, post 요청만 허용
+                                .requestMatchers(HttpMethod.GET, "/api/*/members/logout").permitAll() // 로그인은 누구나 가능, post 요청만 허용
                                 .anyRequest().authenticated()
                 )
                 .csrf(
@@ -37,6 +40,10 @@ public class ApiSecurityConfig {
                 ) // 폼 로그인 방식 끄기
                 .sessionManagement(
                         sessionManagement -> sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                )
+                .addFilterBefore(
+                        jwtAuthorizationFilter, //엑세스 토큰을 이용한 로그인 처리
+                        UsernamePasswordAuthenticationFilter.class
                 );
         ;
         return http.build();
